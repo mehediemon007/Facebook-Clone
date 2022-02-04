baa120a8615971798c15deea002da1fef7a53aa3
mehediemon007 <mehediemon7@gmail.com>
2022-02-04 10:27:53 +0600

    update

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import {Search} from "@mui/icons-material"
 import "./topbar.css";
 
 function Topbar() {
-  return <div>Topbar</div>;
+  return (
+    <div className='topbarContainer'>
+      <div className='topbarLeft'>
+        <span className='logo'>SocialLife</span>
+      </div>
+      <div className='topbarCenter'>
+        <div className='searchbar'>
+          <Search/>
+          <input placeholder='Search for friend, post or video' className='searchInput'/>
+        </div>
+      </div>
+      <div className='topbarRight'>
+        
+      </div>
+    </div>
+  );
 }
 
 export default Topbar;
