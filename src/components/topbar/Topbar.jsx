c9bdfd2729de81ed42dfbad1d946036fc6ee4a64
mehediemon007 <mehediemon7@gmail.com>
2022-02-04 11:42:59 +0600

    navbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -10,7 +10,7 @@ function Topbar() {
       </div>
       <div className='topbarCenter'>
         <div className='searchbar'>
-          <Search/>
+          <Search className='searchIcon'/>
           <input placeholder='Search for friend, post or video' className='searchInput'/>
         </div>
       </div>
@@ -20,21 +20,17 @@ function Topbar() {
           <span className='topbarLink'>Timeline</span>
         </div>
         <div className='topbarIcons'>
-          <div className='topbarIocnItem'>
+          <div className='topbarIconItem'>
             <Person/>
             <span className='topbarIconBadge'>1</span>
           </div>
-          <div className='topbarIocnItem'>
+          <div className='topbarIconItem'>
             <Chat/>
             <span className='topbarIconBadge'>1</span>
           </div>
-          <div className='topbarIocnItem'>
+          <div className='topbarIconItem'>
             <Notifications/>
             <span className='topbarIconBadge'>2</span>
-          </div>
-          <div className='topbarIocnItem'>
-            <Person/>
-            <span className='topbarIconBadge'>1</span>
           </div>
         </div>
         <img src='/assets/person/1.jpeg' alt='user' className='topbarImg'/>
