d3d6870727faf7a31dd7feca6afaa0c8ab842d56
mehediemon007 <mehediemon7@gmail.com>
2022-02-05 11:43:55 +0600

    Ui Complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile"
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login"
 
 function App() {
   return (
       <Home/>
+      // <Profile/>
+      // <Login/>
+      // <Register/>
   );
 }
 
